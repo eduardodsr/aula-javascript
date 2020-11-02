9bfd80ea9499a39f2665160981b991a7ac9e2e72
eduardodsr <eduardodsr@gmail.com>
2020-11-02 12:19:05 -0300

    update main.js

--- aula01/js/main.js
+++ aula01/js/main.js
@@ -1,9 +1,9 @@
 window.alert('Meu primeiro alerta')
 
 function clicou() {
-    window.document.getElementById("agradecimento").innerHTML = "Obrigado"
-    var msg = window.document.getElementById("agradecimento")
+    // window.document.getElementById("agradecimento").innerHTML = "Obrigado"
 
+    var msg = window.document.getElementById("agradecimento")
     msg.innerHTML = "<b>Obrigado</b>!!!"
 }
 
